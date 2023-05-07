081cbc1beff8810f6d2cccc56dadcf660a31d636
pap90 <davidpejuan@unitec.edu>
2023-05-06 23:22:00 -0600

    Calendario
    Se quito la imagen de perfil momentaneamente por la variable fantasma presente en todos los side bars, falta manera de agregar un evento (deje un ejemplo de como agregar un evento como comentario en la linea 5 de Calendario.php) y trabajo en el css

---
+++ Calendar.php
@@ -0,0 +1,80 @@
+<?php
+class Calendar {
+
+    private $active_year, $active_month, $active_day;
+    private $events = [];
+
+    //hace el XML del calendario IMPORTANTE SI NO EL CALENDARIO APARECERA EN ENERO DE 1970!!!
+    public function __construct($date = null) {
+        $this->active_year = $date != null ? date('Y', strtotime($date)) : date('Y');
+        $this->active_month = $date != null ? date('m', strtotime($date)) : date('m');
+        $this->active_day = $date != null ? date('d', strtotime($date)) : date('d');
+    }
+
+    //funcion publica para agregar eventos, es imposible agregar eventos sin esto!!!
+    public function add_event($txt, $date, $days = 1, $color = '') {
+        $color = $color ? ' ' . $color : $color;
+        $this->events[] = [$txt, $date, $days, $color];
+    }
+
+    //esta parte convierte a string las fechas para poder ordenar el calendario, y convierte los numeros en la seccion de dias a literalmente strings represntando dias. e.j: 0=Dom
+    //al igual que hace el conteo del ultimo dia del ultimo mes y el ultimo dia del mes actual para desplegar el calendario dia por dia. y armarlo en html
+    public function __toString() {
+        $num_days = date('t', strtotime($this->active_day . '-' . $this->active_month . '-' . $this->active_year));
+        $num_days_last_month = date('j', strtotime('last day of previous month', strtotime($this->active_day . '-' . $this->active_month . '-' . $this->active_year)));
+        $days = [0 => 'Dom', 1 => 'Lun', 2 => 'Mar', 3 => 'Mie', 4 => 'Jue', 5 => 'Vie', 6 => 'Sab'];
+        $first_day_of_week = array_search(date('D', strtotime($this->active_year . '-' . $this->active_month . '-1')), $days);
+        $html = '<div class="calendar">';
+        $html .= '<div class="header">';
+        $html .= '<div class="month-year">';
+        $html .= date('F Y', strtotime($this->active_year . '-' . $this->active_month . '-' . $this->active_day));
+        $html .= '</div>';
+        $html .= '</div>';
+        $html .= '<div class="days">';
+        //crea los dias 
+        foreach ($days as $day) {
+            $html .= '
+                <div class="day_name">
+                    ' . $day . '
+                </div>
+            ';
+        }
+        for ($i = $first_day_of_week; $i > 0; $i--) {
+            $html .= '
+                <div class="day_num ignore">
+                    ' . ($num_days_last_month-$i+1) . '
+                </div>
+            ';
+        }
+        for ($i = 1; $i <= $num_days; $i++) {
+            $selected = '';
+            if ($i == $this->active_day) {
+                $selected = ' selected';
+            }
+            $html .= '<div class="day_num' . $selected . '">';
+            $html .= '<span>' . $i . '</span>';
+            foreach ($this->events as $event) {
+                for ($d = 0; $d <= ($event[2]-1); $d++) {
+                    if (date('y-m-d', strtotime($this->active_year . '-' . $this->active_month . '-' . $i . ' -' . $d . ' day')) == date('y-m-d', strtotime($event[1]))) {
+                        $html .= '<div class="event' . $event[3] . '">';
+                        $html .= $event[0];
+                        $html .= '</div>';
+                    }
+                }
+            }
+            $html .= '</div>';
+        }
+        for ($i = 1; $i <= (42-$num_days-max($first_day_of_week, 0)); $i++) {
+            $html .= '
+                <div class="day_num ignore">
+                    ' . $i . '
+                </div>
+            ';
+        }
+        $html .= '</div>';
+        $html .= '</div>';
+        return $html;
+    }
+
+}
+?>
